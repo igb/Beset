8521cf8b7eb2370006d9cd5d976ad1654fc11298
Ian Brown <igb@Ian-Browns-MacBook-Pro.local>
2012-03-10 11:06:24 -0800

    initital interface definition

---
+++ beset.erl
@@ -0,0 +1,21 @@
+-module(beset).
+
+-export([init/0, subsets/1, update_set/2, delete_set/1, add_set/2]).
+
+init()->
+    ok.
+
+loop()->
+    ok.
+
+
+subsets(Set)->
+    err.
+
+update_set(Key, Set)->
+    err.
+delete_set(Key)->
+    err.
+
+add_set(Key, Set)->
+    err.
